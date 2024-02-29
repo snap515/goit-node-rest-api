dd31686f70f07732715fc152893d05cb916dd05d
Anton Pedan <77937504+snap515@users.noreply.github.com>
2024-02-29 21:07:11 +0100

    fix bug with DB_HOST import

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from "express";
 import morgan from "morgan";
 import cors from "cors";
 import mongoose from "mongoose";
-import DB_HOST from "./config.js";
 
 import contactsRouter from "./routes/contactsRouter.js";
 

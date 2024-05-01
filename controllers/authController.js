5d4c57fc4612ce1c7c876d04738c7c2576261131
Anton Pedan <77937504+snap515@users.noreply.github.com>
2024-05-01 19:24:06 +0200

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,7 +7,6 @@ import { nanoid } from "nanoid";
 import HttpError from "../helpers/HttpError.js";
 import { sendEmail } from "../helpers/sendEmail.js";
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
-import { emailText } from "../constants/emailText.js";
 
 dotenv.config();
 const { SECRET_KEY, BASE_URL } = process.env;
@@ -31,7 +30,24 @@ const register = async (req, res) => {
   const verifyEmail = {
     to: email,
     subject: "Verify Email",
-    html: emailText,
+    html: `<div style="font-family: Arial, sans-serif;">
+      <h2 style="color: #007bff;">Welcome to Our App!</h2>
+      <p>
+        Hello ${name},<br>
+        Thank you for registering with us!<br>
+        To complete your registration, please click the link below to verify your email address:
+      </p>
+      <p>
+        <a href="${BASE_URL}/api/auth/verify/${verificationCode}" style="color: #28a745; text-decoration: none;">Click here to verify</a>
+      </p>
+      <p>
+        If you did not sign up for our service, you can safely ignore this email.
+      </p>
+      <p>
+        Best regards,<br>
+        The Our App Team
+      </p>
+    </div>`,
   };
 
   await sendEmail(verifyEmail);

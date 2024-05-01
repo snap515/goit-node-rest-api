df47ad8b190caf4da01bc6d169851f342c9c142d
Anton Pedan <77937504+snap515@users.noreply.github.com>
2024-05-01 21:51:58 +0200

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -61,6 +61,8 @@ const register = async (req, res) => {
   });
 };
 
+import { fileURLToPath } from "url";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 

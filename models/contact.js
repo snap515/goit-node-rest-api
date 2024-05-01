af6500ec552a22a45b4ccfef7b3d306751eebf68
Anton Pedan <77937504+snap515@users.noreply.github.com>
2024-05-01 17:29:20 +0200

    temorary canceling email field for contact

--- models/contact.js
+++ models/contact.js
@@ -4,14 +4,14 @@ import { handleMongooseError } from "../helpers/handleMongooseError.js";
 import Joi from "joi";
 export const createContactSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string().required(),
+  // email: Joi.string().required(),
   phone: Joi.number().required(),
   favorite: Joi.boolean(),
 });
 
 export const updateContactSchema = Joi.object({
   name: Joi.string(),
-  email: Joi.string(),
+  // email: Joi.string(),
   phone: Joi.number(),
   favorite: Joi.boolean(),
 }).min(1);
